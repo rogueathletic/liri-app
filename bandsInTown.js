cefefa86b949ceb6065504a4c8f6e2f5f45f472e
Jason Schutz <jason@rogueathletic.com>
2019-02-18 16:30:17 -0800

    Update bandsInTown.js

--- bandsInTown.js
+++ bandsInTown.js
@@ -1,5 +1,6 @@
 var axios = require("axios");
 var fs = require("fs");
+
 function bands(query) {
     var artist = query;
     console.log(artist);
@@ -12,7 +13,7 @@ function bands(query) {
             .then(function (response) {
                 var data = response.data;
                 for (var i = 0; i < data.length; i++) {
-                    console.log("/n                             ");
+                    console.log("                               ");
                     console.log(">----------BANDS--------------<");
                     console.log(">----------IN-----------------<");
                     console.log(">----------TOWN---------------<");
@@ -20,22 +21,26 @@ function bands(query) {
                     console.log(">----------SEARCH-------------<");
                     console.log(">----------RESULTS------------<");
                     console.log("                               ");
-                    console.log('data[i].venue.name:', data[i].venue.name);
-                    console.log('data[i].venue.city:', data[i].venue.city);
-                    console.log('data[i].datetime:', data[i].datetime);
+                    console.log('Venue name: ', data[i].venue.name);
+                    console.log('City: ', data[i].venue.city);
+                    console.log('data & time: ', data[i].datetime);
                     console.log("                               ");
-                    console.log(">----------BANDS--------------<");
-                    console.log(">----------IN-----------------<");
-                    console.log(">----------TOWN---------------<");
-                    console.log(">----------API----------------<");
-                    console.log(">----------SEARCH-------------<");
                     console.log(">----------END----------------<");
-                    console.log("/n                             ");
+                    console.log("                               ");
+                    // fs. code below will append to the log.txt file
+                    fs.appendFileSync("log.txt", "-----------------------\n");
+                    fs.appendFileSync("log.txt", data[i].venue.name);
+                    fs.appendFileSync("log.txt", "-----------------------\n");
+                    fs.appendFileSync("log.txt", data[i].venue.city);
+                    fs.appendFileSync("log.txt", "-----------------------\n");
+                    fs.appendFileSync("log.txt", data[i].datetime);
+                    fs.appendFileSync("log.txt", "-----------------------\n");
                 }
             });
 
     }
 }
+
 function bandIsUndefined(inputParam) {
     if (inputParam === undefined) {
         inputParam = "Backstreet Boys";
@@ -46,16 +51,20 @@ function bandIsUndefined(inputParam) {
         console.log(">----------ANYTHING------------<");
         console.log(">--------- OR------------------<");
         console.log(">----------YOUR----------------<");
-        console.log(">----------MOVIE---------------<");
+        console.log(">----------BAND_---------------<");
         console.log(">----------WASN'T--------------<");
         console.log(">----------FOUND---------------<");
         console.log("                                ");
-        fs.appendFileSync("log.txt", "-----------------------\n"                                                );
-        console.log("If you love awkward old men dancing then you should check out Back Street Boys"                        );
-        fs.appendFileSync("log.txt", "If you love awkward old men dancing then you should check this out" + "\n");
-        console.log("It's awkward!");
-        console.log("              :)                ");
-        fs.appendFileSync("log.txt", "It's awkward!\n");
+        fs.appendFileSync("log.txt", "-----------------------\n");
+        console.log("If you love awkward old men dancing then you should check out Back Street Boys");
+        fs.appendFileSync("log.txt", "If you love awkward old men dancing & singing songs they didnt write then you should check this out: html://bit.ly/B-S__B" + "\n");
+        fs.appendFileSync("log.txt", "-----------------------\n");
+        console.log("so awkward!");
+        console.log("                                    ");
+        console.log("               :)                   ");
+        console.log("                                    ");
+        fs.appendFileSync("log.txt", "They Are awkward!\n");
+        fs.appendFileSync("log.txt", "-----------------------\n");
         return true;
     }
     return false;
